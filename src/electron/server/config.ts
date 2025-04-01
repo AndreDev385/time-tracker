3f1f85053938699175e5dbc9102f06f93e4853af
Andre Izarra <aizarra2015@gmail.com>
2025-03-31 21:38:39 -0400

    :bug: update server api url

--- src/electron/server/config.ts
+++ src/electron/server/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3000/api"
-//export const API_URL = "https://time-tracker-web-kpso.onrender.com/api"
+//export const API_URL = "http://localhost:3000/api"
+export const API_URL = "https://time-tracker-web-kpso.onrender.com/api"
