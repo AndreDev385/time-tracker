b17877725bf656791226e5e408420e86ec0093df
Andre Izarra <programador385@gmail.com>
2025-04-27 17:05:18 -0400

    :sparkles: v0.0.4 for test, uploading user captures

--- src/electron/config.ts
+++ src/electron/config.ts
@@ -5,7 +5,7 @@ dotenv.config()
 export const config = {
 	backBlaze: {
 		key: process.env.BACK_BLAZE_KEY as string,
-		secret: process.env.BACK_BLAZE_SECRETa as string,
+		secret: process.env.BACK_BLAZE_SECRET as string,
 	},
 	nodeEnv: process.env.NODE_ENV,
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import "dotenv/config"
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -18,5 +19,9 @@ export default defineConfig({
   server: {
     port: 5123,
     strictPort: true,
+  },
+  define: {
+    'process.env.BACK_BLAZE_KEY': JSON.stringify(process.env.BACK_BLAZE_KEY),
+    'process.env.BACK_BLAZE_SECRET': JSON.stringify(process.env.BACK_BLAZE_SECRET),
   }
 })
